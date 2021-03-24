6da864799ab862b31f5cee9957cc2d8240344d84
Sytrics <guillaume.leinen@ensta-bretagne.org>
2021-03-24 13:56:09 +0100

    project template

---
+++ parser.py
@@ -0,0 +1,95 @@
+import sys
+from indent import Indent
+
+
+class Parser:
+
+    TYPE = ['INT', 'FLOAT', 'CHAR']
+    STATEMENT_STARTERS = ['SEMICOLON', 'LBRACE', 'IDENTIFIER', 'IF', 'WHILE']
+    REL_OP = ['LT', 'LTE', 'GT', 'GTE']
+    MUL_OP = ['MUL', 'DIV']
+    LITERAL = ['INTEGER_LIT', 'FLOAT_LIT', 'CHAR_LIT']
+
+    def __init__(self, verbose=False):
+        self.indentator = Indent(verbose)
+        self.tokens = []
+        self.errors = 0
+
+    def show_next(self, n=1):
+        try:
+            return self.tokens[n - 1]
+        except IndexError:
+            print('ERROR: no more tokens left!')
+            sys.exit(1)
+
+    def expect(self, kind):
+        actualToken = self.show_next()
+        actualKind = actualToken.kind
+        actualPosition = actualToken.position
+        if actualKind == kind:
+            return self.accept_it()
+        else:
+            print('Error at {}: expected {}, got {} instead'.format(str(actualPosition), kind, actualKind))
+            sys.exit(1)
+
+    # same as expect() but no error if not correct kind
+    def maybe(self, kind):
+        if self.show_next().kind == kind:
+            return self.accept_it()
+
+    def accept_it(self):
+        token = self.show_next()
+        output = str(token.kind) + ' ' + token.value
+        self.indentator.say(output)
+        return self.tokens.pop(0)
+
+    def remove_comments(self):
+        result = []
+        in_comment = False
+        for token in self.tokens:
+            if token.kind == 'COMMENT':
+                pass
+            elif token.kind == 'LCOMMENT':
+                in_comment = True
+            elif token.kind == 'RCOMMENT':
+                in_comment = False
+            else:
+                if not in_comment:
+                    result.append(token)
+        return result
+
+    def parse(self, tokens):
+        self.tokens = tokens
+        self.tokens = self.remove_comments()
+        self.parse_program()
+
+    def parse_program(self):
+        self.indentator.indent('Parsing Program')
+        self.expect('INT')
+        self.expect('IDENTIFIER')
+        self.expect('LPAREN')
+        self.expect('RPAREN')
+        self.expect('LBRACE')
+
+        self.parse_declarations()
+        self.parse_statements()
+
+        self.expect('RBRACE')
+        self.indentator.dedent()
+        if (self.errors == 1):
+            print('WARNING: 1 error found!')
+        elif (self.errors > 1):
+            print('WARNING: ' + str(self.errors) + ' errors found!')
+        else:
+            print('parser: syntax analysis successful!')
+
+    def parse_declarations(self):
+        self.indentator.indent('Parsing Declarations')
+        # TODO
+        self.indentator.dedent()
+
+    def parse_statements(self):
+        self.indentator.indent('Parsing Statements')
+        # TODO
+        self.indentator.dedent()
+ 
