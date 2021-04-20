e07a9b4fb285131fc22ccad55a3351fe447c8e94
sytrics <guillaume.leinen@ensta-bretagne.org>
2021-04-20 09:09:51 +0200

    docstring

--- parser_AST.py
+++ parser_AST.py
@@ -22,6 +22,14 @@ class Parser:
         
 
     def show_next(self, n=1):
+        """
+        Permet d'acceder à un token dans la liste des tokens à consommer
+        Args:
+            n (int, optional): numero du token recherché Defaults to 1.
+
+        Returns:
+            token : le token demandé
+        """
         try:
             return self.tokens[n - 1]
         except IndexError:
@@ -29,6 +37,15 @@ class Parser:
             sys.exit(1)
 
     def expect(self, kind):
+        """
+        Cherche la présence d'un token conformément à la syntaxe python 
+        Args:
+            kind : type de token attendu 
+
+        Returns:
+            appel à accept_it si le token est le bon 
+            syntaxe error sinon 
+        """
         actualToken = self.show_next()
         actualKind = actualToken.type
         actualPosition = (actualToken.ligne, actualToken.position_debut,actualToken.position_fin)
@@ -36,29 +53,30 @@ class Parser:
             if actualKind == kind:
                 return self.accept_it()
             else:
+                print("syntax error")
                 print('Error at {}: expected {}, got {} instead'.format(str(actualPosition), kind, actualKind))
                 sys.exit(1)
         else : 
             if actualKind in kind : 
                  return self.accept_it()
             else:
+                print("syntax error")
                 print('Error at {}: expected {}, got {} instead'.format(str(actualPosition), kind, actualKind))
                 sys.exit(1)
 
-    def accept_it(self):
-        # token = self.show_next()
-        # output = str(token.kind) + ' ' + token.value
+    def accept_it(self) :
         return self.tokens.pop(0)
 
     
-   
-
-    
+#  définitions des fonctions de parsing permettant la création de l'AST.   
         
     def factor(self):
         """
+        Permet de parser les facteurs
         factor = IDENTIFIER | NUMBER | "(" expression ")"
-        .
+        
+        Returns:
+            n : Noeud de l'AST représentant le facteur 
         """
         print("factor")
         retour = []
@@ -76,14 +94,24 @@ class Parser:
             retour.append(self.expression())
             retour.append(Node(self.expect('RPAREN')))
         else:
-            print("factor: syntax error")
-            
+            print("syntax error")
+            actualPosition = (self.show_next().ligne, self.show_next().position_debut,self.show_next().position_fin)
+            print('Error at {}: expected {}, got {} instead'.format(str(actualPosition),"Identifier | number | (expression)", self.show_next().type))
+            sys.exit(1)
+        
+        n = Node()
+        for e in retour:
+            n.addNode(e)
+        return n
 
     #--------------------------------------------------
     def term(self):
         """
+        Permet de parser les termes (combinaison de facteurs)
         term = factor {("*"|"/") factor}
         
+        Returns:
+            n : Noeud de l'AST représentant le terme
         """
         print("term") 
         retour = []
@@ -104,8 +132,11 @@ class Parser:
     #--------------------------------------------------
     def expression(self):
         """
+        Permet de parser les expressions (combinaison linéaire de termes)
         expression = ["+"|"-"] term {("+"|"-") term}
         
+        Returns:
+            n : Noeud de l'AST représentant le terme
         """
         print("expression")
         retour = []
@@ -124,12 +155,16 @@ class Parser:
     #--------------------------------------------------
     def condition(self):
         """
+        Permet de parser une condition (relation entre 2 expression)
+
         condition =
             "odd" expression
             | expression ("="|"#"|"<"|"<="|">"|">=") expression
             | in range () 
             | in ... 
-        .
+        
+        Returns:
+            n : Noeud de l'AST représentant le terme
         """
         print("condition")
         retour = []
@@ -156,11 +191,15 @@ class Parser:
     #--------------------------------------------------
     def statement(self):
         """
+        Permet de parser les statements 
         statement =
             [IDENTIFIER "=" expression
             | "if" condition "then" statement
             | "while" condition "do" statement
             ]
+
+        Returns:
+            n : Noeud de l'AST représentant le terme
         """
         print("statement")
         retour = []
@@ -192,11 +231,14 @@ class Parser:
 
     #--------------------------------------------------
     def parse(self):
-        
+        """
+        Permet le parsing du programme en entier (pas d'entrée vers main)
+        Returns:
+            n : Noeud de l'AST représentant le programme (root)
+        """
         AST = Node()
         while len(self.tokens) > 0 : 
             AST.addNode(self.statement())
-            print(self.show_next())
         print("success : program is compiled")
         return AST
 
